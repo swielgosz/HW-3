9a651d6af3a4df55c63238c2f72070ecb1315105
swielgosz <swielgosz20@gmail.com>
2023-03-23 01:01:14 -0400

    First Attempt w/ State Class

--- hw3main.cpp
+++ hw3main.cpp
@@ -3,122 +3,170 @@
 #include <sstream>
 #include <string>
 #include <iomanip>
-#include "hw2tools.h"
+#include "hw3tools.h"
 #include <string>
 using namespace std;
 
+#define PI 3.14159265359
+
 int main()
 {
+
     // Problem definitions
-    double start_x_arr[5] = {0, 27, 45, -16, 39};
-    double start_y_arr[5] = {0, 30, -45, 10, 5};
-    double goal_x_arr[5] = {-38, -48, -45, 18, 38};
-    double goal_y_arr[5] = {20, 20, 45, -45, -8};
-    double goal_r_arr[5] = {10, 10, 15, 5, 3};
-    double epsilon_arr[5] = {10, 5, 5, 2, 1};
+    double pi = PI;
+    double v_max = 5;
+    double w_max = pi / 2;
+    double a_max = 2;
+    double gamma_max = pi / 2;
+    double delta = .1;
 
-    // Loop through each problem
-    for (int run = 0; run < 5; run++)
+    double start_x = 30;
+    double start_y = -35;
+    double start_theta = pi / 2;
+    double goal_x = 0;
+    double goal_y = 0;
+    double goal_r = 5;
+    double epsilon = 10;
+    
+    // Seed random number generator
+    srand(time(NULL)); 
+
+    // Reading in obstacles
+    ifstream filestream("obstacles.txt");
+    if (!filestream.is_open())
     {
-        double start_x = start_x_arr[run];
-        double start_y = start_y_arr[run];
-        double goal_x = goal_x_arr[run];
-        double goal_y = goal_y_arr[run];
-        double goal_r = goal_r_arr[run];
-        double epsilon = epsilon_arr[run];
-
-        srand(time(NULL)); // seed random number generator
-
-        // Reading in obstacles
-        ifstream filestream("obstacles.txt");
-        if (!filestream.is_open())
-        {
-            cout << "File failed to open" << endl;
-            return 0;
-        }
-
-        int i = 0;
-        double x_ob[20], y_ob[20], r_ob[20];
-        string line;
-        string X, Y, R;
-        while (getline(filestream, line))
-        {
-            stringstream ss(line);
-            getline(ss, X, ',');
-            x_ob[i] = stod(X);
-            getline(ss, Y, ',');
-            y_ob[i] = stod(Y);
-            getline(ss, R, ',');
-            r_ob[i] = stod(R);
-            i++;
-        }
-        int num_ob = i;
-
-        // Start building tree
-        Node *startNode = new Node();
-        startNode->x = start_x;
-        startNode->y = start_y;
-
-        Tree T = Tree();
-        T.add_node(startNode);
-
-        Node *goalNode = NULL;
-        
-        // initialize variables
-        int goal_reached = 0, iter = 0;
-
-        // check to see if we start in goal region
-        goal_reached = goal_check(startNode, goal_x, goal_y, goal_r);
-        if (goal_reached == 1)
-        {
-            cout<<"Start point is already in goal region"<<endl;
-        }
-
-        // Implement RTT
-        int itermax = 10000;
-        while (goal_reached != 1 && iter < itermax)
-        {
-            // select random node from C space
-            Node *randNode = sample();
-
-            // find closest node in tree to the random node
-            Node *closestNode = T.nearest_Node(randNode);
-
-            // find node in direction of random node 1 epsilon away (or shorter if appropriate)
-            Node *solve2BPNode = solve2BP(closestNode, randNode, epsilon);
-
-            // check if this new node is in collision with an obstacle
-            int collision = collision_check(solve2BPNode, closestNode, x_ob, y_ob, r_ob, num_ob);
-
-            // if new node is not in collision, check if it is in the goal region. If not, add it to the search tree
-            if (collision == 0)
-            {
-                T.add_node(solve2BPNode);
-                goal_reached = goal_check(solve2BPNode, goal_x, goal_y, goal_r);
-                if (goal_reached == 1)
-                {
-                    cout << "Goal region reached." << endl;
-                    goalNode = solve2BPNode;
-                    break;
-                }
-            }
-
-            iter++;
-        }
-
-        if (iter >= itermax)
-        {
-            cout<<"Goal region is not reached after "<<itermax<<" iterations"<<endl;
-        }
-
-        // save files that contain the search tree and best path 
-        T.savePathToFile("output_path_" + to_string(run+1) + ".txt", goalNode);
-        T.saveSearchTreeToFile("search_tree_" + to_string(run+1) + ".txt");
+        cout << "File failed to open" << endl;
+        return 0;
     }
+
+    int i = 0;
+    double x_ob[51], y_ob[51], r_ob[51];
+    string line;
+    string X, Y, R;
+    while (getline(filestream, line))
+    {
+        stringstream ss(line);
+        getline(ss, X, ',');
+        x_ob[i] = stod(X);
+        getline(ss, Y, ',');
+        y_ob[i] = stod(Y);
+        getline(ss, R, ',');
+        r_ob[i] = stod(R);
+        i++;
+    }
+    int num_ob = i;
+
+    // Reading in robot points
+    ifstream filestream2("H3_robot.txt");
+    if (!filestream.is_open())
+    {
+        cout << "File failed to open" << endl;
+        return 0;
+    }
+
+    i = 0;
+    string line2, X2, Y2;
+    double x_robot[37], y_robot[37];
+    while (getline(filestream2, line2))
+    {
+        stringstream ss(line2);
+        getline(ss, X2, ',');
+        x_robot[i] = stod(X2);
+        getline(ss, Y2, ',');
+        y_robot[i] = stod(Y2);
+        cout<< "["<<x_robot[i]<<","<<y_robot[i]<<"]"<<endl;
+        i++;
+    }
+    int num_robot_pt = i;
+
+    State* startState = new State();
+    startState->x = start_x;
+    startState->y = start_y;
+    startState->theta = start_theta;
+    startState->v = 0;
+    startState->w = 0;
+    startState->a = 1;
+    startState->gamma = .2;
+    
+    Path initialTrajectory = Path(startState);
+
+    Tree T = Tree();
+    T.add_path(&initialTrajectory);
+
+    // Loop through forward and steering accelerations and create trajectories
+    double da = 0.1; // forward acceleration step
+    double dgamma = pi/10; // steering acceration step
+    int idx1 = 0;
+    int idx2 = 0;
+    int count = 0;
+    std::list<State*> robotStates;
+    std::list<Path> recordPath;
+
+    for (double a = -a_max; a <= a_max; a += da)
+    {
+        for (double gamma = -gamma_max; gamma <= gamma_max; gamma += dgamma )
+        {
+            State* s = new State();
+            s->t = 0;
+            s->x = 0;
+            s->y = 0;
+            s->theta = 0;
+            s->v = 0;
+            s->w = 0;
+            s->a = a;
+            s->gamma = gamma;
+            robotStates.push_back(s);
+
+            Path path = Path(s);
+            path.euler(epsilon,0.1);
+            recordPath.push_back(path);
+           // P.euler(epsilon,0.1);
+            path.saveTrajectoryToFile("./output/hw3/tests/trajectory_" + std::to_string(count) + ".csv");
+            count++;
+
+        }
+    }
+
+
+/*
+    // Start building tree
+    Node *startNode = new Node();
+    startNode->x = start_x;
+    startNode->y = start_y;
+
+    Tree T = Tree();
+    T.add_node(startNode);
+
+    Node *goalNode = NULL;
+*/
+
+
+    
+
     return 0;
 }
 
 ///////////////////////////////// FUNCTIONS //////////////////////////////////////
+
+// random forward acceleration
+double rand_acceleration()
+{
+    double Max = 2.00, Min = -2.00;
+    double rand_a = (double(rand()) / double(RAND_MAX)) * (Max - Min) + Min;
+    return rand_a;
+} 
+
+///////////////////////////////////////////////////////////////////////////////////
+
+// random steering acceleration
+double rand_gamma() 
+{
+    double Max = PI/2, Min = -PI/2;
+    double rand_gamma = (double(rand()) / double(RAND_MAX)) * (Max - Min) + Min;
+    return rand_gamma;
+}
+
+///////////////////////////////////////////////////////////////////////////////////
 
 // Randomly sample from configuration space that is [-50 50] x [-50 50]
 Node *sample()
@@ -144,7 +192,7 @@ double euclidean(Node *Node1, Node *Node2)
 ///////////////////////////////////////////////////////////////////////////////////
 
 // Function to find new node that is in direction of random node from the closest node to it
-Node *solve2BP(Node *closestNode, Node *randNode, double epsilon)
+Node *eulerGoal(Node *closestNode, Node *randNode, double epsilon)
 {
     double x1 = closestNode->x;
     double y1 = closestNode->y;
@@ -177,92 +225,35 @@ Node *solve2BP(Node *closestNode, Node *randNode, double epsilon)
 }
 
 ///////////////////////////////////////////////////////////////////////////////////
+/*
+State *euler(State *currentState, double epsilon, double dt)
+{
+    int iter = 0;
+    double trajectory_distance = 0;
 
-int collision_check(Node *newNode, Node *closestNode, double x_ob[], double y_ob[], double r_ob[], int num_ob)
+    while (trajectory_distance <= epsilon && iter < 10000)
+    {
+        State* newState = new State();
+
+        // currentState->a = rand_a();
+        // currentState->gamma = rand_gamma();
+
+        newState->x = currentState->x + dt * (currentState->v * cos(currentState->theta));
+        newState->y = currentState->y + dt * (currentState->v * sin(currentState->theta));
+        newState->theta = currentState->theta + dt * currentState->w;
+        newState->v = currentState->v + dt * currentState->a;
+        newState->w = currentState->w + dt * currentState->gamma;
+        newState->t = currentState->t + dt;
+
+        this->stateList.push_back(newState);
+
+        trajectory_distance = trajectory_distance + sqrt(pow((newState->x - currentState->x), 2) + pow((newState->y - currentState->y), 2));
+        iter++;
+    }
+}
+/* Euler integration to get trajectory using car dynamics
+Node *euler(Node *closestNode, Node *eulerGoalNode, double delta, double epsilon)
 {
 
-    int point_check = 0;
-    int seg_check = 0;
-    int collision = 0;
-
-    // check if newNode is in any of the objects
-    for (int i = 0; i < num_ob; i++)
-    {
-        // distance from center of object to new node
-        double distance = sqrt(pow(x_ob[i] - newNode->x, 2) + pow(y_ob[i] - newNode->y, 2));
-
-        // if distance is less than radius, the point is in an object
-        if (distance <= r_ob[i])
-        {
-            point_check = 1;
-            break;
-        }
-        else
-        {
-            point_check = 0;
-        }
-    }
-
-    if (point_check == 0)
-    {
-        // check if segment between node 1 and node 2 is in collision with an of the obstacles
-        // algorithm from https://stackoverflow.com/questions/9052507/circle-and-line-segment-intersection user enobayram
-        for (int i = 0; i < num_ob; i++)
-        {
-            double A_x = closestNode->x;
-            double A_y = closestNode->y;
-            double B_x = newNode->x;
-            double B_y = newNode->y;
-            double C_x = x_ob[i];
-            double C_y = y_ob[i];
-            double circle_r = r_ob[i];
-
-            double dx = B_x - A_x;
-            double dy = B_y - A_y;
-            double unit_x = dx / (sqrt(pow(dx, 2) + pow(dy, 2)));
-            double unit_y = dy / (sqrt(pow(dx, 2) + pow(dy, 2)));
-            double d = (C_x - A_x) * unit_y - (C_y - A_y) * unit_x;
-            if (abs(d) < circle_r)
-            {
-                if ((((unit_x * A_x + unit_y * A_y) < (unit_x * C_x + unit_y * C_y)) && ((unit_x * C_x + unit_y * C_y) < (unit_x * B_x + unit_y * B_y))) || (((unit_x * B_x + unit_y * B_y) < (unit_x * C_x + unit_y * C_y)) && ((unit_x * C_x + unit_y * C_y) < (unit_x * A_x + unit_y * A_y))))
-                {
-                    seg_check = 1;
-                    break;
-                }
-            }
-            else
-            {
-                seg_check = 0;
-            }
-        }
-    }
-
-    // if either point or line segment intersects object, collision has ocurred 
-    if (seg_check == 1 || point_check == 1)
-    {
-        collision = 1;
-    }
-    else
-    {
-        collision = 0;
-    }
-    return collision;
 }
-
-///////////////////////////////////////////////////////////////////////////////////
-
-// Check if point is in goal region
-int goal_check(Node *newNode, double goal_x, double goal_y, double goal_r)
-{
-    int goal_check;
-    double distance = sqrt(pow(goal_x - newNode->x, 2) + pow(goal_y - newNode->y, 2));
-    if (distance <= goal_r)
-    {
-        goal_check = 1;
-    }
-    else
-    {
-        goal_check = 0;
-    }
-    return goal_check;
-}
+*/
--- hw3tools.h
+++ hw3tools.h
@@ -8,117 +8,250 @@
 #include <vector>
 #include <string>
 
-
 class Node
 {
-    public:
-        double x;   // physical x location
-        double y;   // physical y location
+public:
+    double x; // physical x location
+    double y; // physical y location
 
-        Node* parentNode;      // used for graph search, this is a pointer to this node's parent
+    Node *parentNode; // used for graph search, this is a pointer to this node's parent
 
-        Node() // constructor
+    Node() // constructor
+    {
+        this->x = 0;
+        this->y = 0;
+        this->parentNode = NULL;
+    }
+};
+
+class State
+{
+public:
+    double x;
+    double y;
+    double theta;
+    double v;
+    double w;
+    double a;
+    double gamma;
+    double t;
+
+    State() // constructor
+    {
+        this->t = 0;
+        this->x = 0;
+        this->y = 0;
+        this->theta = 0;
+        this->v = 0;
+        this->w = 0;
+        this->a = 0;
+        this->gamma = 0;
+    }
+};
+
+class Path
+{
+public:
+
+    std::list<State*> stateList;
+    Path* parent;
+    State* intialPathState;
+
+    Path(State* initialState)
+    {
+        this->stateList = std::list<State*>();
+        this->parent = NULL;  
+        this->intialPathState = initialState;
+        this->stateList.push_back(initialState);
+    }
+
+    void euler(double epsilon, double dt)
+    {
+        int iter = 0;
+        double trajectory_distance = 0;
+
+        while (trajectory_distance <= epsilon && iter < 10000)
         {
-          this->x = 0;
-          this->y = 0;
-          this->parentNode = NULL;
+            State *currentState = this->stateList.back();
+            State *newState = new State();
+
+            // currentState->a = rand_a();
+            // currentState->gamma = rand_gamma();
+
+            newState->x = currentState->x + dt * (currentState->v * cos(currentState->theta));
+            newState->y = currentState->y + dt * (currentState->v * sin(currentState->theta));
+            newState->theta = currentState->theta + dt * currentState->w;
+            newState->v = currentState->v + dt * currentState->a;
+            newState->w = currentState->w + dt * currentState->gamma;
+            newState->t = currentState->t + dt;
+
+            this->stateList.push_back(newState);
+
+            trajectory_distance = trajectory_distance + sqrt(pow((newState->x - currentState->x), 2) + pow((newState->y - currentState->y), 2));
+            iter++;
         }
+    }
+
+    bool saveTrajectoryToFile(std::string TrajectoryFile)
+    {
+        FILE *pFile = fopen(TrajectoryFile.c_str(), "w");
+        if (pFile == NULL)
+        {
+            return false;
+        }
+
+        int vecSize = stateList.size();
+        for (State* const& i : stateList)
+        {
+            fprintf(pFile, "%f, %f, %f, %f, %f, %f\n",
+                    i->t, i->x, i->y, i->theta, i->v, i->w, i->a, i->gamma);
+        }
+        fclose(pFile);
+        printf("saved search tree in %s\n", TrajectoryFile.c_str());
+
+        return true;
+    }
+
+    /*
+            void euler(double epsilon, double dt, Node *eulerGoal)
+        {
+
+            int iter = 0;
+            double trajectory_distance = 0;
+
+            while (trajectory_distance <= epsilon && iter < 10000)
+            {
+                State currentState = this->stateList.back();
+                State newState;
+
+               // currentState.a = rand_a();
+                //currentState.gamma = rand_gamma();
+
+                newState.x = currentState.x + dt * (currentState.v * cos(currentState.theta));
+                newState.y = currentState.y + dt * (currentState.v * sin(currentState.theta));
+                newState.theta = currentState.theta + dt * currentState.w;
+                newState.v = currentState.v + dt * currentState.a;
+                newState.w = currentState.w + dt * currentState.gamma;
+                newState.t = currentState.t + dt;
+
+                this->stateList.push_back(newState);
+
+                trajectory_distance = trajectory_distance + sqrt(pow((newState.x - currentState.x), 2) + pow((newState.y - currentState.y), 2));
+                iter++;
+            }
+
+        }*/
 };
 
 class Tree
 {
-    public:
-        
-        // list of nodes to build tree
-        std::vector<Node*> node_list;
-        
-        // add node to list
-        void add_node(Node *node){
-            node_list.push_back(node);  
-        }
+public:
 
-        // find nearest node to specified input node
-        Node* nearest_Node(Node* newNode)
+    // list of paths to build tree
+    std::list<Path *> pathList;
+
+    // Tree constructor
+    Tree()
+    {
+        this->pathList = std::list<Path*>();
+    }
+
+    // add path to list
+    void add_path(Path *path)
+    {
+        pathList.push_back(path);
+    }
+
+    // list of nodes to build tree
+    std::vector<Node *> node_list;
+
+    // add node to list
+    void add_node(Node *node)
+    {
+        node_list.push_back(node);
+    }
+
+    // find nearest node to specified input node
+    Node *nearest_Node(Node *newNode)
+    {
+        double min_distance = 1000000000000000; // intialize with a really big number ("infinity")
+        unsigned int vecSize = node_list.size();
+        Node *nearestNode = NULL;
+
+        // loop through all nodes to find closest - this is inefficient but still works quickly, sorry :(
+        for (unsigned int i = 0; i < vecSize; i++)
         {
-            double min_distance = 1000000000000000; // intialize with a really big number ("infinity")
-            unsigned int vecSize = node_list.size();
-            Node *nearestNode = NULL;
-
-            // loop through all nodes to find closest - this is inefficient but still works quickly, sorry :( 
-            for (unsigned int i = 0; i<vecSize; i++)
+            Node *currentNode = node_list[i];
+            double distance = sqrt(pow(currentNode->x - newNode->x, 2) + pow(currentNode->y - newNode->y, 2));
+            if (distance < min_distance)
             {
-                Node *currentNode = node_list[i];
-                double distance = sqrt(pow(currentNode->x - newNode->x, 2) + pow(currentNode->y - newNode->y, 2));
-                if(distance < min_distance)
-                {
-                    min_distance = distance;
-                    nearestNode = node_list[i];                    
-                }
+                min_distance = distance;
+                nearestNode = node_list[i];
             }
-            return nearestNode;
         }
+        return nearestNode;
+    }
 
-        //save path (code modified from Dr. Otte)
-        bool savePathToFile(std::string pathFile, Node *goalNode)
+    // save path (code modified from Dr. Otte)
+    bool savePathToFile(std::string pathFile, Node *goalNode)
+    {
+        FILE *pFile = fopen(pathFile.c_str(), "w");
+        if (pFile == NULL)
         {
-            FILE *pFile = fopen(pathFile.c_str(), "w");
-            if (pFile == NULL)
-            {
-                return false;
-            }
-
-            Node *thisNode = goalNode;
-            while (thisNode != NULL)
-            {
-                // format is x, y
-                fprintf(pFile, "%f, %f\n", thisNode->x, thisNode->y);
-                thisNode = thisNode->parentNode;
-            }
-
-            fclose(pFile);
-            printf("saved path in %s\n", pathFile.c_str());
-
-            return true;
+            return false;
         }
 
-        // Save tree (code modified from Dr. Otte)
-        bool saveSearchTreeToFile(std::string searchTreeFile)
+        Node *thisNode = goalNode;
+        while (thisNode != NULL)
         {
-            FILE *pFile = fopen(searchTreeFile.c_str(), "w");
-            if (pFile == NULL)
-            {
-                return false;
-            }
-
-            int vecSize = node_list.size();
-            for (int n = 0; n < vecSize; n++)
-            {
-                Node *thisNode = node_list[n];
-                if (thisNode->parentNode != NULL)
-                {
-
-                    // format is x1, y1, id2, x2, y2
-                    fprintf(pFile, "%f, %f, %f, %f\n",
-                            thisNode->x, thisNode->y,
-                            thisNode->parentNode->x, thisNode->parentNode->y);
-                }
-            }
-            fclose(pFile);
-            printf("saved search tree in %s\n", searchTreeFile.c_str());
-
-            return true;
+            // format is x, y
+            fprintf(pFile, "%f, %f\n", thisNode->x, thisNode->y);
+            thisNode = thisNode->parentNode;
         }
+
+        fclose(pFile);
+        printf("saved path in %s\n", pathFile.c_str());
+
+        return true;
+    }
+
+    // Save tree (code modified from Dr. Otte)
+    bool saveSearchTreeToFile(std::string searchTreeFile)
+    {
+        FILE *pFile = fopen(searchTreeFile.c_str(), "w");
+        if (pFile == NULL)
+        {
+            return false;
+        }
+
+        int vecSize = node_list.size();
+        for (int n = 0; n < vecSize; n++)
+        {
+            Node *thisNode = node_list[n];
+            if (thisNode->parentNode != NULL)
+            {
+
+                // format is x1, y1, id2, x2, y2
+                fprintf(pFile, "%f, %f, %f, %f\n",
+                        thisNode->x, thisNode->y,
+                        thisNode->parentNode->x, thisNode->parentNode->y);
+            }
+        }
+        fclose(pFile);
+        printf("saved search tree in %s\n", searchTreeFile.c_str());
+
+        return true;
+    }
 };
-
 
 // Function prototypes
 
-Node* sample(); // function to randomly sample C space
+Node *sample(); // function to randomly sample C space
 
-int collision_check(Node *, Node*, double x_ob[], double y_ob[], double r_ob[], int num_ob);
+int collision_check(Node *, Node *, double x_ob[], double y_ob[], double r_ob[], int num_ob);
 
-int goal_check(Node*, double goal_x, double goal_y, double goal_r);
+int goal_check(Node *, double goal_x, double goal_y, double goal_r);
 
-Node* solve2BP(Node*, Node*, double);
+Node *solve2BP(Node *, Node *, double);
 
 double euclidean(Node *Node1, Node *Node2);
